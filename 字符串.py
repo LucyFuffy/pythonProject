b8ffe43cf402af84d7109b709f788c82bb2a1e1d
tangchong <123275484@qq.com>
2023-04-24 14:29:35 +0800

    Initial commit

---
+++ 字符串.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+frist_name = "ada"
+last_name = "lovelace"
+full_name = f"{frist_name},{last_name}"
+print(full_name)
